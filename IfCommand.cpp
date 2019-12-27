a55ff42d88094d00ca64a449e5d108f2b831e3d1
hadargold <hadargold1@gmail.con>
2019-12-27 02:25:37 -0800

    hadar

---
+++ IfCommand.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hadar on 12/27/19.
+//
+
+#include "IfCommand.h"
---
+++ IfCommand.h
@@ -0,0 +1,14 @@
+//
+// Created by hadar on 12/27/19.
+//
+
+#ifndef FLIGHTSIMULATOR_IFCOMMAND_H
+#define FLIGHTSIMULATOR_IFCOMMAND_H
+
+
+class IfCommand {
+
+};
+
+
+#endif //FLIGHTSIMULATOR_IFCOMMAND_H
