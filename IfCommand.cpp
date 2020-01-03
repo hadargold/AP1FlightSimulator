54be0361ea879b001f6c3939ebccc0fa3ae8210c
hadargold <50302613+hadargold@users.noreply.github.com>
2020-01-03 15:30:48 +0200

    workingggg

--- IfCommand.cpp
+++ IfCommand.cpp
@@ -4,70 +4,28 @@
 
 #include "IfCommand.h"
 
-
-//void IfCommand::doCommand(vector<string> & lex , int iter) { // should get the lex
-//    iter += 3; // first one was "if"
-//    vector<string> commandsVec;
-//    // now we have all the condition
-//    int breaks = 0;
-//    //it++;
-//    while (!(lex[iter] == "}" && breaks == 0)) {
-//        if (lex[iter] == "{")
-//            breaks++;
-//        if (lex[iter] == "}")
-//            breaks--;
-//        commandsVec.push_back(lex[iter]);
-//        iter++;
-//    }
-//    // check the condition
-//    CommandsManager *cmdMgr = new CommandsManager();
-//    vector <Command*> cmdVec;
-//    Condition *cond = new Condition(conditionVec);
-//    cond->execute(0);
-//    if(cond->getResult())
-//    {
-//        //do commandVec
-//        for(auto i = 0; i< commandsVec.size(); i++)
-//        {
-//            cmdVec.push_back(cmdMgr->commandsFactory(commandsVec, i));
-//        }
-//    }
-//    //it++;
-//}
-
 void IfCommand::execute(int *index) { // should get the lex
-    *index += 1; // first one was "if"
+    (*index) += 1; // first one was "if"
     vector<string> conditionVec, commandsVec;
-    while (lex[iter] != "{") {
-        conditionVec.push_back(lex[iter]);
-        index++;
+    while (lex[*index] != "{") {
+        conditionVec.push_back(lex[*index]);
+        (*index)++;
     }
+
     // now we have all the condition
-    int breaks = 0;
+    int breaks = 1;
+    (*index)++;
     //it++;
-    while (!(lex[iter] == "}" && breaks == 0)) {
-        if (lex[iter] == "{")
+    while (breaks != 0) { //!(lex[*index] == "}" &&
+        if (lex[*index] == "{")
             breaks++;
-        if (lex[iter] == "}")
+        if (lex[*index] == "}")
             breaks--;
-        commandsVec.push_back(lex[iter]);
-        *index +=1;
+        if(breaks != 0)
+            commandsVec.push_back(lex[*index]);
+        (*index) +=1;
     }
-//    // check the condition
-//    CommandsManager *cmdMgr = new CommandsManager();
-//    vector <Command*> cmdVec;
-//    Condition *cond = new Condition(conditionVec);
-//    cond->execute(index);
-//    if(cond->getResult())
-//   {
-//    // do commandVec
-//        for(auto i = 0; i< commandsVec.size(); i++)
-//        {
-//            cmdVec.push_back(cmdMgr->commandsFactory(commandsVec, i));
-//        }
-//    }
-//    //*index +=1;
-//}
+
 
     // check the condition
     CommandsManager *cmdMgr = new CommandsManager();
@@ -77,26 +35,28 @@ void IfCommand::execute(int *index) { // should get the lex
     bool result = false;
     if(conditionVec.size() == 1)
     {
+        //checkkkkkkkkkkkkkkkkkkkkkkk
         result = (conditionVec[0] == "1") ? true : false;
-        *index += 1;
+        //*index += 1;
     }
     else {
         int i = 0;
         int conditionLocation;
-        string left = "";
-        string right = "";
+        string left = conditionVec[0];
+        string right = conditionVec[2];
         Expression *sideA, *sideB;
-        while (conditionVec[i] != "<" && conditionVec[i] != ">"
-               && conditionVec[i] != "<=" && conditionVec[i] != ">="
-               && conditionVec[i] != "!=" && conditionVec[i] != "==")
-        {
-            left += conditionVec[i];
-            i++;
-        }
-        conditionLocation = i;
-        while(i < conditionVec.size()){
-            right += conditionVec[i];
-        }
+        // maybe dont need it
+//        while (conditionVec[i] != "<" && conditionVec[i] != ">"
+//               && conditionVec[i] != "<=" && conditionVec[i] != ">="
+//               && conditionVec[i] != "!=" && conditionVec[i] != "==")
+//        {
+//            left += conditionVec[i];
+//            i++;
+//        }
+//        conditionLocation = i;
+//        while(i < conditionVec.size()){
+//            right += conditionVec[i];
+//        }
 
         Interpreter *stringToInterpretForConditionLeft = new Interpreter();
         SymbolTable *symbolTable = new SymbolTable();
@@ -110,29 +70,29 @@ void IfCommand::execute(int *index) { // should get the lex
         stringToInterpretForConditionLeft->setVariablesByMapOfVars(nameOfVarToVariableMap);
         Expression *expressionRight = stringToInterpretForConditionLeft->interpret(right);
 
-        if(conditionVec[conditionLocation] == ">")
+        if(conditionVec[1] == ">")
             result = expressionLeft->calculate() > expressionRight->calculate() ? true : false;
-        if(conditionVec[conditionLocation] == "<")
+        if(conditionVec[1] == "<")
             result = expressionLeft->calculate() < expressionRight->calculate() ? true : false;
-        if(conditionVec[conditionLocation] == "!=")
+        if(conditionVec[1] == "!=")
             result = expressionLeft->calculate() != expressionRight->calculate() ? true : false;
-        if(conditionVec[conditionLocation] == "==")
+        if(conditionVec[1] == "==")
             result = expressionLeft->calculate() == expressionRight->calculate() ? true : false;
-        if(conditionVec[conditionLocation] == "<=")
+        if(conditionVec[1] == "<=")
             result = expressionLeft->calculate() <= expressionRight->calculate() ? true : false;
-        if(conditionVec[conditionLocation] == ">=")
+        if(conditionVec[1] == ">=")
             result = expressionLeft->calculate() >= expressionRight->calculate() ? true : false;
 
-        *index += 3;
+        //*index += 3;
     }
-
     if(result)
     {
         // do commandVec
         for(auto i = 0; i< commandsVec.size(); i++)
         {
-            cmdVec.push_back(cmdMgr->commandsFactory(commandsVec, i));
+            //cmdVec.push_back(cmdMgr->commandsFactory(commandsVec, i));
+            cmdMgr->commandsFactory(commandsVec, i);
         }
     }
-    //*index +=1;
+    (*index) -=1;
 }
