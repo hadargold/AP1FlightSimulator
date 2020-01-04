daaf6e1009e90fb8c92a4c2a757ceeb40a26bdc7
hadargold <50302613+hadargold@users.noreply.github.com>
2020-01-04 19:43:55 +0200

    4.1

--- WhileCommand.h
+++ WhileCommand.h
@@ -1,14 +1,49 @@
 //
-// Created by hadar on 12/29/19.
+// Created by hadar on 12/27/19.
 //
 
 #ifndef FLIGHTSIMULATOR_WHILECOMMAND_H
 #define FLIGHTSIMULATOR_WHILECOMMAND_H
 
+#include <string>
+#include "Condition.h"
+#include "CommandsManager.h"
 
-class WhileCommand {
-
+//class IfCommand : public Condition{
+class WhileCommand : public Command{
+    //vector<string>::iterator &it;
+    //vector<string> conditionVec;
+    int iter;
+    vector <string> lex;
+public:
+    explicit WhileCommand(int i ,vector <string> lexer) {
+        this->iter = i;
+        this->lex = lexer; // check * !!!!!
+    }
+    void doCommand(vector<string> &text);
+    void execute(int *index);
+    bool calcResult(vector<string> conditionVec);
 };
 
 
 #endif //FLIGHTSIMULATOR_WHILECOMMAND_H
+
+
+////
+//// Created by hadar on 12/29/19.
+////
+//
+//#ifndef FLIGHTSIMULATOR_WHILECOMMAND_H
+//#define FLIGHTSIMULATOR_WHILECOMMAND_H
+//
+//
+//#include "Condition.h"
+//#include "CommandsManager.h"
+//
+//class WhileCommand : Condition {
+//    vector<string>::iterator &it;
+//public:
+//    void doCommand(vector<string> &text);
+//};
+//
+//#endif //FLIGHTSIMULATOR_WHILECOMMAND_H
